89d3f931826bb6cc310155f25b04e267d6d8a689
Quaesto <s5rfcoleg@gmail.com>
2025-09-05 14:21:26 +0300

    feat(server): TCP accept loop + SUBSCRIBE/PING

--- Agent Mesagerie/broker-ruby/bin/broker.rb
+++ Agent Mesagerie/broker-ruby/bin/broker.rb
@@ -1,8 +1,23 @@
-# bin/broker
+# bin/broker (update)
 # !/usr/bin/env ruby
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
+require 'yaml'
 require 'broker/version'
-require 'socket'
-puts "Broker #{Broker::VERSION} starting… (code comes next commits)"
+require 'broker/server'
+require 'broker/dispatch/dispatcher'
+
+cfg = YAML.load_file(File.expand_path('../config/broker.yml', __dir__))
+host = ENV['BROKER_HOST'] || cfg['host']
+port = Integer(ENV['BROKER_PORT'] || cfg['port'])
+workers = Integer(ENV['BROKER_WORKERS'] || cfg['worker_count'])
+dq = Integer(ENV['BROKER_DISPATCH_QUEUE_SIZE'] || cfg['dispatch_queue_size'])
+sq = Integer(ENV['BROKER_SEND_QUEUE_SIZE'] || cfg['send_queue_size'])
+maxb = Integer(ENV['BROKER_MAX_FRAME_BYTES'] || cfg['max_frame_bytes'])
+
+dispatcher = Broker::Dispatch::Dispatcher.new(registry: nil, workers: workers, queue_size: dq)
+# registry va fi creat în Server; dispatcher nu are nevoie aici de el
+
+server = Broker::Server.new(host: host, port: port, max_frame: maxb, send_queue_size: sq, dispatcher: dispatcher)
+server.start
---
+++ Agent Mesagerie/broker-ruby/lib/broker/server.rb
@@ -0,0 +1,78 @@
+# lib/broker/server.rb
+# frozen_string_literal: true
+
+require 'socket'
+require_relative 'connection'
+require_relative 'routing/registry'
+require_relative 'json'
+
+module Broker
+  class Server
+    def initialize(host:, port:, max_frame:, send_queue_size:, dispatcher:)
+      @host = host
+      @port = port
+      @max_frame = max_frame
+      @send_q = send_queue_size
+      @dispatcher = dispatcher
+      @registry = Routing::Registry.new
+      @srv = TCPServer.new(@host, @port)
+      trap('INT') { stop }
+      trap('TERM') { stop }
+    end
+
+    def start
+      puts "Listening on #{@host}:#{@port}"
+      loop do
+        sock = @srv.accept
+        Thread.new { handle(sock) }
+      end
+    rescue IOError, SystemCallError
+      # shutdown
+    end
+
+    def handle(sock)
+      conn = Connection.new(sock, send_queue_size: @send_q)
+      @registry.add(conn)
+      while (raw = conn.read_frame(max: @max_frame))
+        obj = begin
+          Json::LOAD.call(raw)
+        rescue StandardError
+          nil
+        end
+        if obj.nil?
+          conn.send_json!({ 'op' => 'ERROR', 'code' => 'BadRequest', 'detail' => 'invalid JSON' })
+          next
+        end
+        case obj['op']
+        when 'SUBSCRIBE'
+          pats = Array(obj['subjects']).select { |s| s.is_a?(String) && !s.empty? }
+          if pats.empty?
+            conn.send_json!({ 'op' => 'ERROR', 'code' => 'BadRequest', 'detail' => 'subjects required' })
+          else
+            @registry.update(conn, pats)
+            conn.send_json!({ 'op' => 'SUBSCRIBED', 'subjects' => pats })
+          end
+        when 'PING'
+          conn.send_json!({ 'op' => 'PONG' })
+        else
+          # PUBLISH vine în commit-ul următor
+          conn.send_json!({ 'op' => 'ERROR', 'code' => 'BadRequest', 'detail' => 'unknown op' })
+        end
+      end
+    rescue StandardError => e
+      warn "conn_error: #{e.message}"
+    ensure
+      @registry.remove(conn) if conn
+      conn&.close
+    end
+
+    def stop
+      begin
+        @srv.close
+      rescue StandardError
+        nil
+      end
+      exit
+    end
+  end
+end
