11c512cd78b202b8719e8166fa3da050e2a7969a
Mihai <Mihai@DESKTOP-3U2EGAU>
2025-09-09 20:54:22 +0300

    feat: sending json message to broker and receiving messages back

---
+++ Agent Mesagerie/receiver-csharp/Subscriber/Program.cs
@@ -0,0 +1,28 @@
+﻿using Subscriber;
+using System.Net;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Subscriber starting...");
+
+        var subscriber = new SubscriberSocket();
+
+        await subscriber.ConnectAsync(IPAddress.Loopback, 5000);
+
+        if (subscriber.IsConnected)
+        {
+            Console.WriteLine("Enter subscriber ID (ex: sub-1):");
+            string subsriberId = Console.ReadLine() ?? "sub-1";
+
+            string[] subjects = { "order.*" };
+            await subscriber.SubscribeAsync(subjects, subsriberId);
+
+            await subscriber.PingAsync();
+
+        }
+
+        Console.ReadLine();
+    }
+}
