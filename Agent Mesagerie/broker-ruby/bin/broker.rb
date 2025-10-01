e2f086b56750dc4823366b1a40a8d49a866bb2ff
Quaesto <s5rfcoleg@gmail.com>
2025-10-01 10:26:44 +0300

    refactor(broker): deleted the TCP old logic

--- Agent Mesagerie/broker-ruby/bin/broker.rb
+++ Agent Mesagerie/broker-ruby/bin/broker.rb
@@ -16,16 +16,13 @@ workers = Integer(ENV.fetch('BROKER_WORKERS', cfg['worker_count'] || 4))
 dq = Integer(ENV.fetch('BROKER_DISPATCH_QUEUE_SIZE', cfg['dispatch_queue_size'] || 10_000))
 subscriber_buffer = Integer(ENV.fetch('BROKER_SUBSCRIBER_BUFFER', cfg['subscriber_buffer'] || 100))
 
-redis_url = ENV['REDIS_URL']
-redis_prefix = ENV['REDIS_STREAM_PREFIX'] || 'stream:messages:'
-store = nil
-if redis_url
-  require 'broker/storage/redis_store'
-  store = RedisStore.new(url: redis_url, prefix: redis_prefix)
-else
-  require 'broker/storage/in_memory_store'
-  store = InMemoryStore.new
-end
+redis_url = ENV['REDIS_URL'] || cfg['redis_url']
+redis_prefix = ENV['REDIS_STREAM_PREFIX'] || cfg['redis_stream_prefix'] || 'stream:messages:'
+
+raise 'REDIS_URL must be configured for the broker' unless redis_url
+
+require 'broker/storage/redis_store'
+store = RedisStore.new(url: redis_url, prefix: redis_prefix)
 server = Broker::Server.new(host: host,
                             port: port,
                             worker_count: workers,
