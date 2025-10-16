798fc84011ab782b58cded45088e6f3c8e298cf4
Mihai <Mihai@DESKTOP-3U2EGAU>
2025-10-16 14:06:11 +0300

    feat: added entities

---
+++ SmartProxy/Server/DataLayer/Entities/Document.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DataLayer.Entities
+{
+    public abstract class Document
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = null!;
+
+        [BsonElement("LastChangedAt")]
+        public DateTime LastChangedAt { get; set; } = DateTime.UtcNow;
+    }
+}
