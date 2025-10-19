9b43918d50b77a04471b27409c8fefcd12161d68
Mihai <Mihai@DESKTOP-3U2EGAU>
2025-10-20 01:39:07 +0300

    [feat]: added an http client for testing a concurent request

---
+++ SmartProxy/HttpClientDemo/HttpClientDemo/EmployeeGetDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HttpClientDemo
+{
+    public class EmployeeGetDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public decimal Salary { get; set; }
+        public string Position { get; set; } = string.Empty;
+        public DateTime LastChangedAt { get; set; }
+    }
+}
