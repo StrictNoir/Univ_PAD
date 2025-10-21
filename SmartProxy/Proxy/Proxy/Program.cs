e7c03a77d525f4eaf51da971a1f34d90a699bf5c
Mihai <Mihai@DESKTOP-3U2EGAU>
2025-10-21 19:45:34 +0300

    [feat]: added in memory cache in proxy

--- SmartProxy/Proxy/Proxy/Program.cs
+++ SmartProxy/Proxy/Proxy/Program.cs
@@ -1,10 +1,17 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json",optional: false,reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration)
+    .AddCacheManager(settings =>
+    {
+       
+        settings.WithDictionaryHandle();
+    });
 builder.Logging.AddConsole();
 
 var app = builder.Build();
