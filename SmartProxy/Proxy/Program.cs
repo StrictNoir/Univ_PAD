43db0fe28db3a1f0762fe0fad0f64d7f90d046fe
Petru Golban <petrugolban4@gmail.com>
2025-10-22 11:40:23 +0300

    added proxy project w cashing

---
+++ SmartProxy/Proxy/Program.cs
@@ -0,0 +1,17 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json",optional: false,reloadOnChange: true);
+builder.Services
+    .AddOcelot(builder.Configuration)
+    .AddCacheManager(x => { x.WithDictionaryHandle(); });
+
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+await app.UseOcelot();
+
+app.Run();
