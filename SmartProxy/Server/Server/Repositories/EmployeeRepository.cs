fa3932c29e080677148f2cda1ddc70bd3675a25b
Petru Golban <petrugolban4@gmail.com>
2025-11-10 20:10:18 +0200

    fixed change to postgresql

--- SmartProxy/Server/Server/Repositories/EmployeeRepository.cs
+++ SmartProxy/Server/Server/Repositories/EmployeeRepository.cs
@@ -1,11 +1,15 @@
 ﻿using DataLayer.Entities;
-using MongoDB.Driver;
+using System.Data;
+using Microsoft.Extensions.Logging;
 
 namespace Server.Repositories
 {
-    public interface IEmployeeRepository: IRepository<Employee> { }
-    public class EmployeeRepository : Repository<Employee>,IEmployeeRepository
+    public interface IEmployeeRepository : IRepository<Employee> { }
+
+    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
     {
-        public EmployeeRepository(IMongoDatabase db, ILogger<Repository<Employee>> logger) : base(db,logger) { }
+        public EmployeeRepository(IDbConnection db, ILogger<Repository<Employee>> logger) 
+            : base(db, logger) 
+        { }
     }
 }
